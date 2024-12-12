eca81c7aae7e9706de1f716bd82d3808124999a1
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-13 06:45:54 +0900

    ♻️  コンポーネントの分割

---
+++ 04_react_basic/src/070_component_nest/start/components/Child.jsx
@@ -0,0 +1,13 @@
+import "./Child.css";
+import List from "./list"
+
+export default () => {
+  return (
+    <div className="component">
+      <h3>Hello Component</h3>
+      <List />
+    </div>
+  );
+};
+
+
---
+++ 04_react_basic/src/070_component_nest/start/components/List.jsx
@@ -0,0 +1,18 @@
+const List = () => {
+  return (
+    <ul>
+      <li>item-1</li>
+      <li>item-2</li>
+      <li>item-3</li>
+      <li>item-4</li>
+      <li>item-5</li>
+    </ul>
+  )
+};
+
+const a = '名前付きExportのサンプル変数'
+// 名前付きのExport 
+export default List;
+
+// export default list
+
