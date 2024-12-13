01842d9557aa5ee0b2878af5789cbe63aabffe5b
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-14 06:06:30 +0900

    ✨ フラグメント
    Add	新規(ファイル)機能追加
    Update	機能修正、既存機能追加（バグではない)
    Clean	リファクタリング
    Remove	削除など
    Fix	バグ修正
    Upgrade	バージョンアップ

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,19 @@
-import "./Child.css";
+import { Fragment } from 'react';
+import './Child.css';
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis odit
+        animi sapiente ullam rem, quasi eum autem illo! Magni impedit similique
+        quisquam! Doloremque, quos quasi officia neque officiis sequi ad!
+      </p>
+    </Fragment>
   );
 };
 
