9693526e5eaa104d7bdaa960ba114f96ed4f0862
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-15 06:36:27 +0900

    Childrenの利用方法

--- 04_react_basic/src/110_props_children/start/Example.jsx
+++ 04_react_basic/src/110_props_children/start/Example.jsx
@@ -1,15 +1,35 @@
-import Profile from "./components/Profile";
-import Container from "./components/Container";
+import Profile from './components/Profile';
+import Container from './components/Container';
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: 'Takashi', age: 19, country: 'Japan' },
+  { name: 'Jane', age: 28, country: 'UK', color: 'red' },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="1: Childrenをネストで記述">
+        {/* ネストして記述する事で、ContainerのchildrenとしてProfileコンポーネントを渡す事ができる */}
+        <Profile {...profile[0]} />
+        <Profile {...profile[1]} />
+      </Container>
+
+      <Container
+        title="2: Childrenを配列で記述"
+        // keyを指定しないと、ブラウザ側で以下エラーがコンソールに出力される
+        // Example.jsx:18 Warning: Each child in a list should have a unique "key" prop.
+        children={[
+          <Profile key={profile[0].name} {...profile[0]} />,
+          <Profile key={profile[1].name} {...profile[1]} />,
+        ]}
+      />
+
+      <Container
+        title="Childrenを利用せず、別名かつ個別に記述"
+        first={<Profile {...profile[0]} />}
+        second={<Profile {...profile[1]} />}
+      />
     </div>
   );
 };
--- 04_react_basic/src/110_props_children/start/components/Container.jsx
+++ 04_react_basic/src/110_props_children/start/components/Container.jsx
@@ -1,9 +1,17 @@
-import "./Container.css";
+import './Container.css';
 
-const Container = ({ title }) => {
+const Container = ({
+  title,
+  children /** Example.jsxでネストしたProfileがchildren props として取得できる*/,
+  first,
+  second,
+}) => {
   return (
     <div className="container">
       <h3>{title}</h3>
+      {children}
+      {second}
+      {first}
     </div>
   );
 };
