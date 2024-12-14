b1a3e92bc8f56ccda52632cba978feb51f845c67
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-15 06:34:59 +0900

    propsで様々な値を渡す

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,11 +1,29 @@
 import Child from './components/Child';
 
 const Example = () => {
+  const hello = (arg) => {
+    return `Hello ${arg}`;
+  };
+
+  const o = {
+    color: 'red',
+    num: 123,
+  };
+
   return (
     <>
-      <Child color="" />
-      <Child color="red" />
-      <Child />{' '}
+      <Child
+        // 外でobjectとして定義して、スプレッド公文で代入する事も可能
+        {...o}
+        // color="red"
+        // num={12345}
+        fn={hello}
+        bool
+        obj={{ name: 'Taro', age: 33 }}
+      />
+      {/* <Child color="" /> */}
+      {/* <Child color="red" /> */}
+      {/* <Child num={12345} fn={hello} bool /> */}
       {/* 属性を指定しないと、props でデフォルトとして定義していている内容が反映する */}
     </>
   );
--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -8,10 +8,15 @@ import './Child.css';
 {
   /* default値を'greenとして設定' */
 }
-const Child = ({ color = 'green' }) => {
+const Child = ({ color = 'green', num, fn, bool, obj }) => {
   return (
     <div className={`component ${color}`}>
       <h3>Hello Component</h3>
+      <h3>{fn('Function')}</h3>
+      <h3>{num}</h3>
+      <h3>{bool ? 'true' : 'false'}</h3>
+      <h3>{obj.name}</h3>
+      <h3>{obj.age}</h3>
     </div>
   );
 };
