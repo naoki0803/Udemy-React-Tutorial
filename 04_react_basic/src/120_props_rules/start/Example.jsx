32e00522bb35c9f8c412b53c1c0a6d2762bc61a1
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-15 06:58:40 +0900

    propsのルール

--- 04_react_basic/src/120_props_rules/start/Example.jsx
+++ 04_react_basic/src/120_props_rules/start/Example.jsx
@@ -1,14 +1,17 @@
 // POINT propsの流れは一方通行
+// 子コンポーネントのHelloで定義したnameを兄弟のByeにわたす事は基本的にできない
 // POINT propsは読み取り専用
 
-import Bye from "./components/Bye"
-import Hello from "./components/Hello"
+import Bye from './components/Bye';
+import Hello from './components/Hello';
+
+const name = 'Taro';
 
 const Example = () => {
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name} />
+      <Bye name={name} />
     </>
   );
 };
--- 04_react_basic/src/120_props_rules/start/components/Hello.jsx
+++ 04_react_basic/src/120_props_rules/start/components/Hello.jsx
@@ -1,4 +1,10 @@
 const Hello = (props) => {
+  console.log('🚀🚀🚀 ~ Hello ~ props:', props.name); // 'Taro'
+
+  // propsを書き換えようとするとエラーになる
+  // TypeError: Cannot assign to read only property 'name' of object '#<Object>'
+  // props.name = 'Jiro';
+
   return (
     <div>
       <h3>Hello {props.name}</h3>
