e879925deaaff05070bffd058f22fc9f30d0853b
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-15 14:18:52 +0900

    onClick Event

--- 05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ 05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,17 @@
 const Example = () => {
+  const onClickHandler = () => {
+    alert('ボタンがクリックされました');
+  };
+
   return (
-    <></>
+    <>
+      <button onClick={onClickHandler}>button1</button>
+      {/** 直接アロー関数の処理を記述してもいいが、可読性が悪いので、別で関数を定義したほうがよい */}
+      {/* prettier-ignore */}
+      <button onClick={() => {alert('ボタンがクリックされました');}}>
+        button2
+      </button>
+    </>
   );
 };
 
