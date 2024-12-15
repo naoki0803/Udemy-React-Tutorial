15913c48902166b362c5b895996d689100a008f1
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-16 06:39:16 +0900

    複数のステートを利用する

--- 05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ 05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+
+// let [countA, setCountA] = useState(0); // useStateはComponentのTopレベルでないと呼ぶことができない。(ここはNG)
+
 const Example = () => {
-  
+  // if (true) {
+  //   let [countA, setCountA] = useState(0); // useStateはComponentのTopレベルでないと呼ぶことができない。(ここはNG)
+  // }
+
+  // useStateはComponentのTopレベルでないと呼ぶことができない。 (ここはOK)
+  let [countA, setCountA] = useState(0);
+  let [countB, setCountB] = useState(10);
+  let [countC, setCountC] = useState(100);
+  return (
+    <>
+      <p>ボタンを{countA}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountA(countA + 1);
+        }}
+      >
+        ボタンA
+      </button>
+
+      <p>ボタンを{countB}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountB(countB + 1);
+        }}
+      >
+        ボタンB
+      </button>
+
+      <p>ボタンを{countC}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountC(countC + 1);
+        }}
+      >
+        ボタンC
+      </button>
+    </>
+  );
 };
 
 export default Example;
