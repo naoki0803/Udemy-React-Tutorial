eeae4061f0df968059c2cb58c524b7f1a4e4d7f5
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-17 08:49:10 +0900

    statet更新時の注意と、即時更新方法

--- 05_state_and_event/src/050_prev_state/start/Example.jsx
+++ 05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  
+  const [count, setCount] = useState(0);
+  const countUp = () => {
+    setCount(count + 1);
+    // setCountを複数回呼ぶ場合、関数を実行することでレンダリング前に即時的にcountの値を更新できる
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+  };
+  const countDown = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <p>現在のカウント数: {count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </>
+  );
 };
 
 export default Example;
