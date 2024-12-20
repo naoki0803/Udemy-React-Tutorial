2ebd33a6c59216e7eb68b6c7c0ec626acc3eb254
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-21 07:21:46 +0900

    if && ?? の利用方法

--- 06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ 06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ['Dog', 'Cat', null, 'Rat'];
 
-  const [filterVal, setFilterVal] = useState("");
+  const [filterVal, setFilterVal] = useState('');
 
   return (
     <>
@@ -13,15 +13,50 @@ const Example = () => {
         onChange={(e) => setFilterVal(e.target.value)}
       />
       <ul>
-        {animals
-          .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
-            return isMatch;
-          })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+        {
+          animals
+            .filter((animal) => {
+              const animalStr = animal ?? '';
+              return animalStr.indexOf(filterVal) !== -1;
+            })
+
+            // if文
+            // .map((animal) => {
+            //   if (animal === 'Dog') {
+            //     return <li key={animal}>{animal + '★'}</li>;
+            //   } else {
+            //     return <li key={animal}>{animal}</li>;
+            //   }
+            // })
+
+            // 三項演算子(冗長1)
+            // .map((animal) => {
+            //   return (
+            //     <li key={animal}>{animal === 'Dog' ? animal + '★' : animal}</li>
+            //   );
+            // })
+
+            // // 三項演算子(冗長2) ※画面に表示しない''(空文字)が入っている
+            // .map((animal) => {
+            //   return (
+            //     <li key={animal}> {animal + (animal === 'Dog' ? '★' : '')}</li>
+            //   );
+            // })
+
+            // 三項演算子(DRY かつ && 表記)
+            .map((animal) => {
+              return (
+                <li key={animal}>
+                  {/* + 演算子の場合 animal === 'Dog' の結果がfalseの場合、文字列でfalseを表示してしまう*/}
+                  {/* {animal + (animal === 'Dog' && '★')} */}
+                  {/* reactで記述した場合booleanは画面に表示されないので、以下記述であれば、Dogの場合のみ★が表示される */}
+                  {animal ?? 'Null | undefined'} {animal === 'Dog' && '★'}
+                </li>
+              );
+            })
+
+          // ?? (null | undefined)
+        }
       </ul>
     </>
   );
