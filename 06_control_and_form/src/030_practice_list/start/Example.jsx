b5c0d133d313d1d1dd030797905599ab080fb40f
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-21 05:45:03 +0900

    リストにkeyを渡す練習問題

--- 06_control_and_form/src/030_practice_list/start/Example.jsx
+++ 06_control_and_form/src/030_practice_list/start/Example.jsx
@@ -1,11 +1,37 @@
+import Profile from './components/Profile';
+
+class Person {
+  constructor(name, age, hobbies) {
+    this.name = name;
+    this.age = age;
+    this.hobbies = hobbies;
+  }
+}
+
 const Example = () => {
+  const taro = new Person('taro', 15, ['sports', 'music']);
+  const jiro = new Person('jiro', 33, ['test', 'サウナ']);
+  const saburo = new Person('saburo', 20, ['寝る', '映画']);
+
+  const persons = [];
+  persons.push(taro, jiro, saburo);
+
   return (
     <>
-      <h3>練習問題</h3>
-      <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
-      <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
+      {/* <h3>練習問題</h3>
+      <p>
+        Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。
+      </p>
+      <p>
+        また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。
+      </p> */}
+
       <ul>
-        {/* ここに記述 */}
+        {persons.map((person) => (
+          <li key={person.name}>
+            <Profile {...person} />
+          </li>
+        ))}
       </ul>
     </>
   );
