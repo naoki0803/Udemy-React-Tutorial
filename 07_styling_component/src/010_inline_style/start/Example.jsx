5eb58f42d63331765453df03558936990e906ed0
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-28 05:56:51 +0900

    インラインスタイルでCSSを記述する場合

--- 07_styling_component/src/010_inline_style/start/Example.jsx
+++ 07_styling_component/src/010_inline_style/start/Example.jsx
@@ -1,16 +1,39 @@
-import { useState } from "react"
+/** inLineでStyleを当てる方法
+ *
+ * 1. objectとしてstyleを記述する
+ * 2. jsxの要素内に直接styleを記述する
+ *
+ */
+
+import { useState } from 'react';
 
 const Example = () => {
-    const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
-    const clickHandler = () => setIsSelected(prev => !prev);
+  const clickHandler = () => setIsSelected((prev) => !prev);
 
-    return (
-        <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
-        </>
-    )
+  const style = {
+    width: 120,
+    height: 60,
+    display: 'block',
+    fontWeight: 'bold',
+    'border-radius': 9999, // css表記にする場合は文字列で記述する
+    cursor: 'pointer',
+    margin: 'auto',
+    background: isSelected ? 'pink' : '',
+  };
+
+  return (
+    <>
+      <button onClick={clickHandler} style={style}>
+        ボタン
+      </button>
+      {/* 要素に直接記述する場合、波カッコを重複して表現する */}
+      <div style={{ textAlign: 'center' }}>
+        {isSelected && 'クリックされました。'}
+      </div>
+    </>
+  );
 };
 
 export default Example;
