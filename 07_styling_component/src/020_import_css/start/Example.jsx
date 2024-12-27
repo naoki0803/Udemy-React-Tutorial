2dde6d1117f69d83ab7d441fb4b287fbe29ecbdd
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-28 06:33:08 +0900

    外部CSSの読み込みと注意点

--- 07_styling_component/src/020_import_css/start/Example.jsx
+++ 07_styling_component/src/020_import_css/start/Example.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
+/** 外部CSSの読み込む方法
+ *
+ * 1. importでフィルを読み込む
+ * 2. 適用したい要素に className={'css内のセレクター'}
+ *
+ * 注意点
+ * 外部CSS読み込みの場合、コンポーネントをまたいで、
+ * CSSに記述されているセレクターを利用している全ての要素で共通のCSSが適用される。
+ *
+ * 例として、SubButton コンポーネント内のSubButton.cssのみ、
+ * bg-colorをorangeに設定しているが、start、end内のすべてclassName={'btn'}にorangeが反映してしまう。
+ */
+
+import SubButton from './components/SubButton';
+import './Example.css';
+import { useState } from 'react';
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
-  const style = {
-    margin: "auto",
-    "border-radius": "9999px",
-    border: "none",
-    display: "block",
-    width: 120,
-    height: 60,
-    fontWeight: "bold",
-    cursor: "pointer",
-    backgroundColor: isSelected ? "pink" : "",
-  };
-
   return (
     <>
-      <button style={style} onClick={clickHandler}>
+      <button
+        // className={`btn ${isSelected && 'selected'}`}  //テンプレートリテラル無いで&&を利用するとfalseの場合CSSにfalseが文字列でわたってしまうので利用しない
+        className={`btn ${isSelected ? 'selected' : ''}`}
+        onClick={clickHandler}
+      >
         ボタン
       </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
+
+      <SubButton />
+      <div style={{ textAlign: 'center' }}>
+        {isSelected && 'クリックされました。'}
       </div>
     </>
   );
