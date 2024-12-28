84c628ff6458b7275c9157b487326f7f2fd89611
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-29 07:30:18 +0900

    css module の説明編集

--- 07_styling_component/src/030_css_module/start/components/CssModuleButton.jsx
+++ 07_styling_component/src/030_css_module/start/components/CssModuleButton.jsx
@@ -1,8 +1,12 @@
-/** Css Module
+/** css Module とは
  *
- * 1. 読み込むCSSファイルの名前に'module'を追加する ※｢ファイル名.module.css｣
- *  ※moduleをつけないと、エラーになる。
- * 2. cssファイルをimportするさいに、 import styles や classes と名前をつけて、読み込む
+ * CSS Moduleは、CSSのスコープをコンポーネント単位で管理するための技術です。
+ * 通常のCSSでは、クラス名がグローバルスコープで適用されるため、
+ * 異なるコンポーネント間でクラス名が衝突する可能性があります。CSS Moduleを使用すると、
+ * 各コンポーネントに対してユニークなクラス名が自動的に生成されるため、スタイルの衝突を防ぐことができます。
+ *
+ * 1. CSSファイルをインポートする際に、import styles from './filename.module.css';のように、
+ *    オブジェクトとしてインポートします。
  *
  * module.cssは、class名に独自のclass名が自動的に付与されるので、
  * 外部読み込みCSSと切り分けたcssを適用する事ができる。
