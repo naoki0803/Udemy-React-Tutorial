14e0a4ef568e9d99f9858bed1c08224081cdc654
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-12-31 14:58:40 +0900

    css in js の記述方法

--- 07_styling_component/src/040_css_in_js/start/Example.jsx
+++ 07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -1,4 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
+import styled from 'styled-components';
+
+console.dir(styled);
+
+// タブ付きテンプレートの表記で、関数を実行していることと同じ。
+const StyledButton = styled.button`
+  margin: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${({ isSelected }) => isSelected ? 'pink' : ''};
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,14 +23,17 @@ const Example = () => {
 
   return (
     <>
+      <StyledButton isSelected={isSelected} onClick={clickHandler}>
+        ボタン
+      </StyledButton>
       <button
-        className={`btn ${isSelected ? "selected" : ""}`}
+        className={`btn ${isSelected ? 'selected' : ''}`}
         onClick={clickHandler}
       >
         ボタン
       </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
+      <div style={{ textAlign: 'center' }}>
+        {isSelected && 'クリックされました。'}
       </div>
     </>
   );
