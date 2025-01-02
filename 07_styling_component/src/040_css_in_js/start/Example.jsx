4a2b92f61508d30f3ba8ff9f697593f225733e6e
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-03 06:36:47 +0900

    css in js の利用方法

--- 07_styling_component/src/040_css_in_js/start/Example.jsx
+++ 07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -1,3 +1,19 @@
+/**CSS-in-Jsとは
+ * 
+ * JavaScript内にCSSを記述する記法
+ * 基本的にLib(styledCssなど)を入れて利用する。
+ * CSS-moduleと同様に一意のクラス名が付与されるので、コンポーネント単位で疎結合な状態にできる
+ * 疑似要素や疑似セレクターを利用することも可能
+ * 
+ * 以下のように記述することで、テンプレートテラル内で、CSSを記述する事ができる
+ * styled.タグ名``
+ * ※タグ付きテンプレートと言う記法で、実際には関数を実行している。
+ * 
+ * また、定義したCSSを継承して、利用する事も可能
+ * 継承時は移管ように記述して、テンプレートリテラル内で、オーバーライドをする
+ * styled(継承元変数名)``
+*/
+
 import { useState } from 'react';
 import styled from 'styled-components';
 
@@ -16,6 +32,25 @@ const StyledButton = styled.button`
   background: ${({ isSelected }) => isSelected ? 'pink' : ''};
 `;
 
+// StyledButtonを継承した、オレンジボタンの作成
+const OrangeButton = styled(StyledButton)`
+  background-color: orange;
+
+  :hover {
+    color: red;
+    opacity: 0.5;
+  }
+
+  // ネストしてCSSを記述することも可能
+  span {
+    font-size: 1.5em;
+  }
+
+  @media (max-width: 600px) {
+    color:blue;
+  }
+`;
+
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
@@ -24,13 +59,17 @@ const Example = () => {
   return (
     <>
       <StyledButton isSelected={isSelected} onClick={clickHandler}>
-        ボタン
+        StyledButton
       </StyledButton>
+
+      <OrangeButton isSelected={isSelected} onClick={clickHandler}>
+      <span>オレンジボタン(継承)</span>
+      </OrangeButton>
       <button
         className={`btn ${isSelected ? 'selected' : ''}`}
         onClick={clickHandler}
       >
-        ボタン
+        <span>ボタン</span>
       </button>
       <div style={{ textAlign: 'center' }}>
         {isSelected && 'クリックされました。'}
