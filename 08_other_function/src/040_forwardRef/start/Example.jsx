a5c3381649a7909085b978411c042a1e32d6d0aa
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-08 16:53:34 +0900

    refの受け渡し方法(forwardRef)

--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,12 +1,32 @@
 import { useRef, forwardRef } from "react";
 
+const Input = ({ customRef }) => {
+    return <input type="text" ref={customRef} />;
+};
+
+// forwardRef関数として定義することで、refを子要素に渡す事ができる。
+// callback関数の第二引数に、refがわたってくる
+const ForwardInput = forwardRef((props, ref) => {
+    return <input type="text" ref={ref} />;
+});
+
 const Example = () => {
     const ref = useRef();
+    const customRef = useRef();
     return (
         <>
-            <input type="text" ref={ref} />
+            {/* ref ではなく、別名称でrefを渡すことでエラーが解消される */}
+            <div>
+                <Input customRef={customRef} />
+            </div>
+            <button onClick={() => customRef.current.focus()}>
+                Input(上段)インプット要素をフォーカスする
+            </button>
+            <div>
+                <ForwardInput ref={ref} />
+            </div>
             <button onClick={() => ref.current.focus()}>
-                インプット要素をフォーカスする
+                ForwardInput(下段)にインプット要素をフォーカスする
             </button>
         </>
     );
