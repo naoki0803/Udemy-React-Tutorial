69dd6c47081ce3b8fa0e5e8406742f175019f2d2
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-08 17:15:28 +0900

    useImperativeHandleの利用方法

--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,13 +1,26 @@
-import { useRef, forwardRef } from "react";
+import { useRef, forwardRef, useImperativeHandle } from "react";
 
 const Input = ({ customRef }) => {
     return <input type="text" ref={customRef} />;
 };
 
 // forwardRef関数として定義することで、refを子要素に渡す事ができる。
+// 結果的に親コンポーネントから、子コンポーネントのDOMを操作する事ができる
 // callback関数の第二引数に、refがわたってくる
+// 子から親に対してデータがわたっていくので、基本的には利用しない方がよい。(useStateなどで実装する)
 const ForwardInput = forwardRef((props, ref) => {
-    return <input type="text" ref={ref} />;
+    // useImperativeHandle
+    // 第一引数にわたってきたref、第二引数に、使用したいメソッドを含むObjectを含む関数
+    // 親コンポーネントかわわたってきたrefと子コンポーネント内で利用するrefは別の物を定義する必要がある。
+    const inputRef = useRef();
+
+    useImperativeHandle(ref, () => ({
+        myFocus() {
+            inputRef.current.focus();
+            console.log("focus");
+        },
+    }));
+    return <input type="text" ref={inputRef} />;
 });
 
 const Example = () => {
@@ -25,7 +38,9 @@ const Example = () => {
             <div>
                 <ForwardInput ref={ref} />
             </div>
-            <button onClick={() => ref.current.focus()}>
+            {/* rcurrentにDOMが入ってくるため、子コンポーネント作成者の意図と異なる挙動が実行される可能性がある */}
+            {/* <button onClick={() => ref.current.focus()}> */}
+            <button onClick={() => ref.current.myFocus()}>
                 ForwardInput(下段)にインプット要素をフォーカスする
             </button>
         </>
