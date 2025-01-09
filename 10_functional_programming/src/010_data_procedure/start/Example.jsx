3f1cb8814ac82bc2c08b64fb105ffd37247c1191
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-10 05:32:53 +0900

    ｢状態管理と処理を分離｣について宣言型と手続型の対比

--- 10_functional_programming/src/010_data_procedure/start/Example.jsx
+++ 10_functional_programming/src/010_data_procedure/start/Example.jsx
@@ -1,10 +1,38 @@
 const Example = () => {
-  return (
-    <>
-      <h3>状態管理と処理を分離</h3>
-      <p>状態（データ）と処理（やりたいこと）は切り離す</p>
-    </>
-  );
+    // POINT 関数型
+    // （値の）状態と処理を分離して管理
+    // A(data) -> B(data) -> C(data) -> 結果
+    // ★ 状態と処理は切り離す
+    const nums = [1, 2, 3];
+    const sum = (arry) => arry.reduce((accu, curr) => accu + curr);
+
+    // POINT オブジェクト指向型
+    // 状態（データ）と処理を対で管理
+    // obj.method(); -> 結果
+
+    const numObj = {
+        nums: [1, 2, 3],
+        sum() {
+            const nums = this.nums;
+            let sumValue = 0;
+
+            for (let i = 0; i < nums.length; i++) {
+                sumValue += nums[i];
+            }
+
+            return sumValue;
+        },
+    };
+
+    return (
+        <>
+            <h3>状態管理と処理を分離</h3>
+            <p>状態（データ）と処理（やりたいこと）は切り離す</p>
+
+            <p>手続き型: numObj.sum()で記述 → {numObj.sum()}</p>
+            <p>関数型き型: sum(nums)で記述 → {sum(nums)}</p>
+        </>
+    );
 };
 
 export default Example;
