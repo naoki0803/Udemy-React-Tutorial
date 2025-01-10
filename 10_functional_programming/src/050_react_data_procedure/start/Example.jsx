066decc5377eb1dae9d8a12218298dcb837783df
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-11 06:10:48 +0900

    reactで実装する関数型プログラミング

--- 10_functional_programming/src/050_react_data_procedure/start/Example.jsx
+++ 10_functional_programming/src/050_react_data_procedure/start/Example.jsx
@@ -1,16 +1,33 @@
+/**関数型プログラミングの観点でreactをどのように記述するか */
+
+// 関数型（純粋関数）とは
+// POINT 純粋関数は数式のようなもので、fn(決まった引数) に対して、 決まった戻り値を返す
+// 上記を満たす為に以下3つを実行する
+//  1. 関数外の状態（データ）は参照・変更しない。
+//  2. 関数外に影響を及ぼさない。
+//  3. 引数で渡された値を変更しない。
+
 import { useState } from "react";
 
+let value = 0;
+
 const Example = () => {
-  const [ state, setState ] = useState(0);
-  const increment = () => {
-    setState(state + 1);
-  }
-  return (
-    <>
-      <button onClick={increment}>+</button>
-      <h3>{state}</h3>
-    </>
-  );
+    const [state, setState] = useState(0);
+    const increment = () => {
+        setState(state + 1);
+    };
+
+    // React では以下のような記述はNG
+    let sumValue = value * 2; // 1. 関数外の状態（データ）は参照・変更しない。
+    window.alert(sumValue); // 2. 関数外に影響を及ぼさない。
+
+    return (
+        <>
+            <button onClick={increment}>+</button>
+            <h3>{state}</h3>
+            {/* <p>{sumValue}</p> */}
+        </>
+    );
 };
 
 export default Example;
