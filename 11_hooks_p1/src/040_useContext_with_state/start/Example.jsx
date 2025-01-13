88fe0c3bf541a105d03d5750b80cd3660b4291b8
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-14 06:10:52 +0900

    stateをuseContextで管理する

--- 11_hooks_p1/src/040_useContext_with_state/start/Example.jsx
+++ 11_hooks_p1/src/040_useContext_with_state/start/Example.jsx
@@ -1,31 +1,39 @@
-/** useContextとは
+/** useState の値を useContext で管理する
  *
- * propsのバケツリレーをせず、任意のコンポーネントで定義した変数を任意のコンポーネントで利用することができる。
  *
- * 利用方法
- * <useContextの定義元  ※今回は Example.jsx>
- * 1-1. reactから createContext を import する。
- * 1-2. createContext()の引数にデフォルト値を指定して、変数を定義し export する。
+ * useContext の挙動
+ * 1. useContext() が実行されると、親コンポーネントを順番に遡る
+ * 2-1. Provider が見つかった場合、 Provider.value の値を取得する
+ * 2-2. Provider が見つからなかった場合、 createContext(initialValue) の初期値を取得する
  *
- * <useContextの利用先  ※今回は GrandChild.jsx>
- * 2-1. react から useContext を import する。
- * 2-2. 上記1-2で定義した変数を import する。
- * 2-3. useContext(2-2でimportした変数名)を実行すると、記述したコンポーネントで1-2で定義した変数を利用できる。
+ * useContext の実装 (Provider を利用する方法)
+ * <定義元>
+ *  1-1. createContext()で定義した <変数(オブジェクト).Provider> で、利用先のコンポーネントを囲む
+ *  1-2. 1-1 で定義した<変数(オブジェクト).Provider value={参照したいstate}> と記述する
+ *  ※今回の場合、<MyContext.Provider value={[value, setValue]}> と記述
  *
- * 例:
- * const value = useContext(MyContext); // MyContextから値を取得
+ * <利用先>
+ *  2-1. useContext() で利用したいcontextを取得し、実装する
+ *  ※今回は、以下実装をしている
+ *    1. <OtherChild />で setState の更新関数を読み込み、+ ボタンクリック時に state を更新する実装
+ *    2･ <GrandChild />で 上記で更新される state を実装
  */
 
-import { createContext } from "react"; // 1-1
+import { createContext, useState } from "react";
 import Child from "./components/Child";
 import OtherChild from "./components/OtherChild";
-export const MyContext = createContext("hello"); //exportする // 1-2
+export const MyContext = createContext();
 
 const Example = () => {
+    // state を useContext で管理する場合、state の定義元と、
+    // 利用先をいずれも記述しているコンポーネントで useState() を実装する必要がある。
+    const [value, setValue] = useState(0);
     return (
         <>
-            <Child />
-            <OtherChild />
+            <MyContext.Provider value={[value, setValue]}>
+                <Child />
+                <OtherChild />
+            </MyContext.Provider>
         </>
     );
 };
