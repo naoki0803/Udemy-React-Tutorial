8b50c05a3dccf60c0c06f77360d6524be7185feb
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-21 06:05:14 +0900

    useEffectの依存配列の挙動

--- 12_hooks_p2/src/020_useEffect_update/start/Example.jsx
+++ 12_hooks_p2/src/020_useEffect_update/start/Example.jsx
@@ -1,20 +1,46 @@
 import { useEffect, useState } from "react";
 
-const Example = () => {
-  const [time, setTime] = useState(0);
+/** 依存配列について
+ * 依存配列に含めたstateが更新されると、
+ * useEffect内のコールバック関数が再実行される
+ */
 
-  useEffect(() => {
-    console.log('useEffect is called');
-    window.setInterval(() => {
-      setTime(prev => prev + 1);
-    }, 1000);
-  }, [])
-  
-  return (
-    <h3>
-      <time>{time}</time>
-      <span>秒経過</span>
-    </h3>
+const Example = () => {
+    const [time, setTime] = useState(0);
+    const upDateTime = () => {
+        console.log("upDateTime is called");
+        window.setInterval(() => {
+            setTime((prev) => prev + 1);
+        }, 1000);
+    };
+
+    // 依存配列が空なので、初回レンダリング時のみ upDateTime が実行される
+    useEffect(() => {
+        upDateTime();
+    }, []);
+
+    // 依存配列に time が入っているので、初回事項された upDateTime 内で1秒毎に time が更新されるたびに実行される
+    useEffect(() => {
+        console.log("useEffect upDate");
+    }, [time]);
+
+    // その他のサンプルコード
+    useEffect(() => {
+        document.title = "counter:" + time;
+        window.localStorage.setItem("time-key", time);
+    }, [time]);
+
+    // useEffect 利用時の注意点
+    // useEffect 内で更新関数を呼ぶと、無限ループになるので実施しない。
+    useEffect(() => {
+        // setTime((prev) => prev + 1);
+    }, [time]);
+
+    return (
+        <h3>
+            <time>{time}</time>
+            <span>秒経過</span>
+        </h3>
     );
 };
 
