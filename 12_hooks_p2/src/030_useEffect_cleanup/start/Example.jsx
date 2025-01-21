9f0800324ab6edf935966453ae5bb80395919a46
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-22 06:19:33 +0900

    useEffect の return とクリーンアップ について

--- 12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
+++ 12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
@@ -1,25 +1,58 @@
 import { useEffect, useState } from "react";
 
+/** useEffect の return について
+ *  useEffect の return に設定した関数は、以下タイミングで実施される
+ *    1. useEffect が記載されているコンポーネントが削除時
+ *    2. 依存配列で指定した値が更新されと時
+ *
+ */
+
 const Example = () => {
-  const [time, setTime] = useState(0);
+    const [isDisplay, setIsDisplay] = useState(true);
+    return (
+        <>
+            {isDisplay && <Timer />}
+            <button onClick={() => setIsDisplay((prev) => !prev)}>
+                トグル
+            </button>
+        </>
+    );
+};
 
-  useEffect(() => {
-    console.log('useEffect is called');
-    window.setInterval(() => {
-      setTime(prev => prev + 1);
-    }, 1000);
-  }, [])
-  
-  useEffect(() => {
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
-  }, [time]);
+const Timer = () => {
+    const [time, setTime] = useState(0);
 
-  return (
-    <h3>
-      <time>{time}</time>
-      <span>秒経過</span>
-    </h3>
+    useEffect(() => {
+        // console.log("init");
+        let intervalId = null;
+        intervalId = window.setInterval(() => {
+            console.log("interval called");
+            setTime((prev) => prev + 1);
+        }, 1000);
+
+        return () => {
+            // コンポーネントが削除されたタイミングで intervalId を clear している
+            // この処理が無いと、コンポーネントが削除された後に、裏で setInterval が動き続けてしまう。
+            window.clearInterval(intervalId);
+        };
+    }, []);
+
+    useEffect(() => {
+        // console.log("updated");
+        document.title = "counter:" + time;
+        window.localStorage.setItem("time-key-end", time);
+
+        return () => {
+            // debugger;
+            // console.log("updated end");
+        };
+    }, [time]);
+
+    return (
+        <h3>
+            <time>{time}</time>
+            <span>秒経過</span>
+        </h3>
     );
 };
 
