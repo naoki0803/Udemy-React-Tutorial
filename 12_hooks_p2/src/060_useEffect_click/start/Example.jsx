8b8a8146a07f19b38f94670f6d3c0ba43294ac94
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-24 06:30:38 +0900

    useEffect 練習問題

--- 12_hooks_p2/src/060_useEffect_click/start/Example.jsx
+++ 12_hooks_p2/src/060_useEffect_click/start/Example.jsx
@@ -1,56 +1,84 @@
 import { useEffect, useState, useLayoutEffect } from "react";
 
 const Example = () => {
-  const [isDisp, setIsDisp] = useState(true);
+    const [isDisp, setIsDisp] = useState(true);
+    const [isStop, setIsStop] = useState(true);
+    const [time, setTime] = useState(0);
 
-  return (
-    <>
-      {isDisp && <Timer/>}
-      <button onClick={() => setIsDisp(prev => !prev)}>トグル</button>
-    </>
-  )
-}
+    // 初期状態は Timerが停止している
+    // スタートをクリックすると、Timer がスタートする
+    // リセットをクリックすると、Timer の値が0になる
 
-const Timer = () => {
-  const [time, setTime] = useState(0);
+    const handleReset = () => {
+        window.localStorage.setItem("time-key", setTime(0));
+    };
 
-  useEffect(() => {
-    // console.log('init');
-    let intervalId = null;
-    intervalId = window.setInterval(() => {
-      // console.log('interval running');
-      setTime(prev => prev + 1);
-    }, 1000);
-    return () => {
-      window.clearInterval(intervalId)
-      // console.log('end');
-    }
-  }, [])
-  
-  useEffect(() => {
-    // console.log('updated');
-    
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key', time);
+    const handleStart = () => {
+        setIsStop((prev) => !prev);
+    };
 
-    return () => {
-      // debugger
-      // console.log('updated end');
-    }
-  }, [time]);
+    return (
+        <>
+            {isDisp && <Timer state={[time, setTime]} isStop={isStop} />}
+            {isDisp && (
+                <button onClick={handleStart}>
+                    {isStop ? "スタート" : "一時停止"}
+                </button>
+            )}
+            {isDisp && <button onClick={handleReset}>リセット</button>}
+            <button
+                style={{ display: "block" }}
+                onClick={() => setIsDisp((prev) => !prev)}
+            >
+                {isDisp ? "非表示" : "表示"}
+            </button>
+        </>
+    );
+};
 
-  useLayoutEffect(() => {
-    const _time = parseInt(window.localStorage.getItem('time-key'));
-    if(!isNaN(_time)) {
-      setTime(_time);
-    }
-  }, [])
+const Timer = ({ state, isStop }) => {
+    // debugger;
+    const [time, setTime] = state;
 
-  return (
-    <h3>
-      <time>{time}</time>
-      <span>秒経過</span>
-    </h3>
+    useEffect(() => {
+        // console.log('init');
+        let intervalId = null;
+        intervalId = window.setInterval(() => {
+            // console.log("interval running");
+            if (!isStop) {
+                setTime((prev) => prev + 1);
+            }
+        }, 1000);
+        return () => {
+            window.clearInterval(intervalId);
+            // console.log('end');
+        };
+    }, [isStop]);
+
+    useEffect(() => {
+        // console.log('updated');
+
+        document.title = "counter:" + time;
+        window.localStorage.setItem("time-key", time);
+
+        return () => {
+            // debugger
+            // console.log('updated end');
+        };
+    }, [time]);
+
+    useLayoutEffect(() => {
+        const _time = parseInt(window.localStorage.getItem("time-key"));
+        if (!isNaN(_time)) {
+            setTime(_time);
+        }
+    }, []);
+
+    return (
+        <h3>
+            <time>{time}</time>
+            <span>秒経過</span>
+        </h3>
     );
 };
 
