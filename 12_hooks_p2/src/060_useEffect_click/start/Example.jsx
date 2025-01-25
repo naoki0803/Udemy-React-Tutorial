e07e3b7aa464b6cc7df9b74d29b4ecd0cc318a58
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-25 14:06:13 +0900

    useEffect練習問題 restHandlerの挙動修正

--- 12_hooks_p2/src/060_useEffect_click/start/Example.jsx
+++ 12_hooks_p2/src/060_useEffect_click/start/Example.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useLayoutEffect } from "react";
 
 const Example = () => {
     const [isDisp, setIsDisp] = useState(true);
-    const [isStop, setIsStop] = useState(true);
+    const [isRun, setIsRun] = useState(false);
     const [time, setTime] = useState(0);
 
     // 初期状態は Timerが停止している
@@ -11,18 +11,19 @@ const Example = () => {
 
     const handleReset = () => {
         window.localStorage.setItem("time-key", setTime(0));
+        setIsRun(false);
     };
 
     const handleStart = () => {
-        setIsStop((prev) => !prev);
+        setIsRun((prev) => !prev);
     };
 
     return (
         <>
-            {isDisp && <Timer state={[time, setTime]} isStop={isStop} />}
+            {isDisp && <Timer state={[time, setTime]} isRun={isRun} />}
             {isDisp && (
                 <button onClick={handleStart}>
-                    {isStop ? "スタート" : "一時停止"}
+                    {isRun ? "一時停止" : "スタート"}
                 </button>
             )}
             {isDisp && <button onClick={handleReset}>リセット</button>}
@@ -36,7 +37,7 @@ const Example = () => {
     );
 };
 
-const Timer = ({ state, isStop }) => {
+const Timer = ({ state, isRun }) => {
     // debugger;
     const [time, setTime] = state;
 
@@ -45,7 +46,7 @@ const Timer = ({ state, isStop }) => {
         let intervalId = null;
         intervalId = window.setInterval(() => {
             // console.log("interval running");
-            if (!isStop) {
+            if (isRun) {
                 setTime((prev) => prev + 1);
             }
         }, 1000);
@@ -53,7 +54,7 @@ const Timer = ({ state, isStop }) => {
             window.clearInterval(intervalId);
             // console.log('end');
         };
-    }, [isStop]);
+    }, [isRun]);
 
     useEffect(() => {
         // console.log('updated');
