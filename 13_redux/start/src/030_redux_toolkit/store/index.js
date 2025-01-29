2b6af9c4ddb6c1b0f6105c310c8fc59847de9386
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-01-30 06:30:23 +0900

    RTKを用いたredux~

--- 13_redux/start/src/030_redux_toolkit/store/index.js
+++ 13_redux/start/src/030_redux_toolkit/store/index.js
@@ -1,9 +1,31 @@
-import { createStore, combineReducers } from "redux";
-import { reducer } from "./modules/counter"
+// import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { reducer } from "./modules/counter";
+import reducer from "./modules/counter";
 
 
-const reducers = combineReducers({
-  counter: reducer,
-});
+/** reduxの場合の記述
+ * 複数の reducer を登録する為に、
+ * combineReducers() の引数に登録したい reducer をオブジェクトで渡す
+ */
+// const reducers = combineReducers({
+//   counter: reducer,
+//   counter2: reducer2,
+// });
+// export default createStore(reducers);
 
-export default createStore(reducers);
+
+/** reduxToolKit (移行RTK) の場合
+ * はじめから複数のreducerを渡す設計になっており、
+ * configureStore の引数にオブジェクトとして reducer を渡し、
+ * その値にさらにオブジェクトとして、複数の reducer を記述する
+ * 
+ * 実際に reducer を定義する場合、RTK の createSlice を利用する
+ * * import { createSlice } from '@reduxjs/toolkit'
+*/
+// 
+export default configureStore({
+  reducer: {
+    counter: reducer
+  }
+})
--- 13_redux/start/src/030_redux_toolkit/store/modules/counter.js
+++ 13_redux/start/src/030_redux_toolkit/store/modules/counter.js
@@ -1,28 +1,60 @@
-const initialState = 0;
+import { createSlice } from "@reduxjs/toolkit";
 
-const reducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "counter/+":
+/** RTK を用いて reducer を定義する
+ * 1. createSlice をimportする
+ * 2. createSliceの引数にオブジェクトを定義し、変数に代入する
+ *    name:hoge         ※configureStore内で reducer のオブジェクトにわたすプロパティ名になる
+ *    initialState: 0   ※state の初期値
+ *    reducers:{}       ※更新する為の`関数`を記述する
+ * 3. 2で代入した変数のactionsプロパティから、reducers:{} 内で記述した更新関数を、分割代入で取得する
+ *    ※actions プロパティ (ActionCreator) は TRK の場合自動で作成される
+ */
+
+const counter = createSlice({
+  name: "counter",
+  initialState: 0,
+  reducers: {
+    add(state, { type, payload } /* actionから分割代入で取得している */) {
       return state + payload;
-    case "counter/-":
+    },
+    minus(state, { type, payload }) {
       return state - payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-const add = (payload) => {
-    return {
-        type: "counter/+",
-        payload
-    }
-}
+const { add, minus } = counter.actions;
 
-const minus = (payload) => {
-    return {
-        type: "counter/-",
-        payload
-    }
-}
 
-export { reducer, add, minus }
+/** redux で reducer を定義する
+ *  1. reducer を定義
+ *  2. actionCreator を記述する
+ */
+// const initialState = 0;
+// const reducer = (state = initialState, { type, payload }) => {
+//   switch (type) {
+//     case "counter/+":
+//       return state + payload;
+//     case "counter/-":
+//       return state - payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// const add = (payload) => {
+//   return {
+//     type: "counter/+",
+//     payload,
+//   };
+// };
+
+// const minus = (payload) => {
+//   return {
+//     type: "counter/-",
+//     payload,
+//   };
+// };
+
+export { add, minus };
+export default counter.reducer;
