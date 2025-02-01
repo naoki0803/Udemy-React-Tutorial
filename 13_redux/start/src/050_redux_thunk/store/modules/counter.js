eb1b62d3f766340017956a13a3d5cdc5833b821e
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-01 20:17:40 +0900

    追加漏れファイル

--- 13_redux/start/src/050_redux_thunk/store/modules/counter.js
+++ 13_redux/start/src/050_redux_thunk/store/modules/counter.js
@@ -25,26 +25,24 @@ const counter = createSlice({
 const { add, minus } = counter.actions;
 
 /** redux-thunkの定義
-  const thunkFunction = (payload) => {
-    return (dispatch, getState) => {
-      副作用処理  
+    const thunkFunction = (payload) => {
+        return (dispatch, getState) => {
+            副作用処理  
+        }
     }
-  }
-
-  redux-thunkは戻り値に関数を返す
-  第一引数に、dispatch  
-  第に引数に、getState
-
-  関数内の処理で非同期などの副作用を伴う処理を記述し、
-  その結果を第一引数の dispatch で値を更新する処理を行う。
-
-  原則として、redux内の reducer 関数内で非同期処理などを行うことは、
-  関数の純粋制を損なうため記述できない。
-
-  そのため、middleware として redux-thunk を用いて非同期処理などの副作用を記述する必要がある。
-  ※middleware内では副作用を伴う処理を記述してOK
+    
+    redux-thunkは戻り値に関数を返す
+    第一引数に、dispatch  
+    第に引数に、getState
+    関数内の処理で非同期などの副作用を伴う処理を記述し、
+    その結果を第一引数の dispatch で値を更新する処理を行う。
+    原則として、redux内の reducer 関数内で非同期処理などを行うことは、
+    関数の純粋制を損なうため記述できない。
+    そのため、middleware として redux-thunk を用いて非同期処理などの副作用を記述する必要がある。
+    ※middleware内では副作用を伴う処理を記述してOK
 
 */
+
 const addAsync = (payload) => {
     return async (dispatch, getState) => {
         // const state = getState();
