ef7234d19aae55d81e0829e3c99199952d82ec36
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-01 20:38:41 +0900

    非同期処理のステータスを表示させる

--- 13_redux/start/src/060_createAsyncThunk/store/modules/counter.js
+++ 13_redux/start/src/060_createAsyncThunk/store/modules/counter.js
@@ -1,39 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { asyncCount } from "../../api/counter"
+import { asyncCount } from "../../api/counter";
 
 const counter = createSlice({
-  name: 'counter',
-  initialState: {
-    count: 0
-  },
-  reducers: {
-    add(state, { type, payload }) {
-      state.count = state.count + payload;
-      // const newState = { ...state };
-      // newState.count = state.count + payload
-      // return newState;
+    name: "counter",
+    initialState: {
+        count: 0,
+        status: "",
     },
-    minus(state, { type, payload }) {
-      state.count = state.count - payload;
-      // const newState = { ...state };
-      // newState.count = state.count - payload
-      // return newState;
-    }
-  }
+    reducers: {
+        add(state, { type, payload }) {
+            state.count = state.count + payload;
+            // const newState = { ...state };
+            // newState.count = state.count + payload
+            // return newState;
+        },
+        minus(state, { type, payload }) {
+            state.count = state.count - payload;
+            // const newState = { ...state };
+            // newState.count = state.count - payload
+            // return newState;
+        },
+    },
+    // 条件分岐が生じる state の更新
+    extraReducers: (builder) => {
+        builder
+            .addCase(addAsyncWithStatus.pending, (state) => {
+                state.status = "Loading...";
+            })
+            .addCase(addAsyncWithStatus.fulfilled, (state, action) => {
+                state.status = "取得済";
+                state.count = state.count + action.payload;
+            })
+            .addCase(addAsyncWithStatus.rejected, (state) => {
+                state.status = "エラー";
+            });
+    },
 });
 
 const { add, minus } = counter.actions;
 
+const addAsyncWithStatus = createAsyncThunk(
+    "counter/asyncCount",
+    async (payload) => {
+        const response = await asyncCount(payload);
+        return response.data;
+    }
+);
 const addAsync = (payload) => {
-  return async (dispatch, getState) => {
-    const state = getState();
-    console.log(state);
-    const response = await asyncCount(payload);
-    dispatch(add(response.data));
-  }
-}
+    return async (dispatch, getState) => {
+        const state = getState();
+        console.log(state);
+        const response = await asyncCount(payload);
+        dispatch(add(response.data));
+    };
+};
 
-
-export { add, minus, addAsync }
-export default counter.reducer
+export { add, minus, addAsync, addAsyncWithStatus };
+export default counter.reducer;
