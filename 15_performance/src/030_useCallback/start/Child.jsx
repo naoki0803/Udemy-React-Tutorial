02358808cfd6569b59db2fc070731dee7ed8bc97
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-05 06:20:29 +0900

    useCallbackを利用した関数のメモ化

--- 15_performance/src/030_useCallback/start/Child.jsx
+++ 15_performance/src/030_useCallback/start/Child.jsx
@@ -1,22 +1,44 @@
+/**
+ * memo化をしても、props の onClick は親コンポーネントで定義されており、
+ * 親コンポーネントが再レンダリングされる度に、関数が再定義され、
+ * 元のonClickと中身は同じだが、メモリ空間の異なる別の関数として渡される。
+ * 結果的に、memo化をしていても、props が変更されたとみなされて、再レンダリングが発生する。
+ */
 import { memo } from "react";
 
 const ChildMemo = memo(({ countB, onClick }) => {
-  console.log("%cChild render", "color: red;");
+    console.log("%cChild render", "color: red;");
 
-  return (
-    <div className="child">
-      <h2>子コンポーネント領域</h2>
-      <div>
-        <button
-          onClick={onClick}
-        >
-          ボタンB
-        </button>
-        <span>子のpropsに渡すstateを更新</span>
-      </div>
-      <span>ボタンBクリック回数：{countB}</span>
-    </div>
-  );
+    return (
+        <div className="child">
+            <h2>子コンポーネント領域</h2>
+            <div>
+                <button onClick={onClick}>ボタンB</button>
+                <span>
+                    メモ化しているが、ボタンAをクリックすると再レンダリングされる
+                </span>
+            </div>
+            <span>ボタンBクリック回数：{countB}</span>
+        </div>
+    );
 });
 
-export default ChildMemo;
+const CallBackChildMemo = memo(({ countB, onClick }) => {
+    console.log("%cCallBackChildMemo render", "color: blue;");
+
+    return (
+        <div className="child">
+            <h2>子コンポーネント領域</h2>
+            <div>
+                <button onClick={onClick}>ボタンB</button>
+                <span>
+                    useCallback
+                    を定義する事で、ボタンAをクリックしても再レンダリングされない
+                </span>
+            </div>
+            <span>ボタンBクリック回数：{countB}</span>
+        </div>
+    );
+});
+
+export { ChildMemo, CallBackChildMemo };
--- 15_performance/src/030_useCallback/start/Example.jsx
+++ 15_performance/src/030_useCallback/start/Example.jsx
@@ -1,37 +1,50 @@
 import React, { useState } from "react";
-import Child from "./Child";
+import { ChildMemo, CallBackChildMemo } from "./Child";
+import { useCallback } from "react";
 
 const Example = () => {
-  console.log("Parent render");
-  
-  const [countA, setCountA] = useState(0);
-  const [countB, setCountB] = useState(0);
+    console.log("Parent render");
 
-  const clickHandler = () => {
-    setCountB((pre) => pre + 1);
-  }
+    const [countA, setCountA] = useState(0);
+    const [countB, setCountB] = useState(0);
 
-  return (
-    <div className="parent">
-      <div>
-        <h3>親コンポーネント領域</h3>
-        <div>
-          <button
-            onClick={() => {
-              setCountA((pre) => pre + 1);
-            }}
-          >
-            ボタンA
-          </button>
-          <span>親のstateを更新</span>
+    /**
+     * 子コンポーネント内で、memo化をしてイルが、props で渡している clickHandler は、
+     * 親コンポーネント(本コンポーネント)で定義しており、
+     * 親コンポーネント(本コンポーネント)が再レンダリングされる度に、関数が再定義され、
+     * 元の clickHandler と中身は同じだが、メモリ空間の異なる別の関数として props で渡される。
+     * 結果的に、memo化をしていても、props が変更されたとみなされて、再レンダリングが発生する。
+     */
+    const clickHandler = () => {
+        setCountB((pre) => pre + 1);
+    };
+
+    const callbackClickHandler = useCallback(() => {
+        setCountB((pre) => pre + 1);
+    }, []);
+
+    return (
+        <div className="parent">
+            <div>
+                <h3>親コンポーネント領域</h3>
+                <div>
+                    <button
+                        onClick={() => {
+                            setCountA((pre) => pre + 1);
+                        }}
+                    >
+                        ボタンA
+                    </button>
+                    <span>親のstateを更新</span>
+                </div>
+            </div>
+            <div>
+                <p>ボタンAクリック回数：{countA}</p>
+            </div>
+            <ChildMemo countB={countB} onClick={clickHandler} />
+            <CallBackChildMemo countB={countB} onClick={callbackClickHandler} />
         </div>
-      </div>
-      <div>
-          <p>ボタンAクリック回数：{countA}</p>
-      </div>
-      <Child countB={countB} onClick={clickHandler}/>
-    </div>
-  );
+    );
 };
 
 export default Example;
