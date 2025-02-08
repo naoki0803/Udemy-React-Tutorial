3923cb133dd1ae778b2e3d01c023d7cf36e60b0f
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-09 05:44:11 +0900

    useDeferredValueを利用した遅延処理

--- 15_performance/src/070_useDeferredValue/start/Example.jsx
+++ 15_performance/src/070_useDeferredValue/start/Example.jsx
@@ -1,37 +1,41 @@
-import { useState, useTransition } from "react";
+import { useState, useTransition, useDeferredValue } from "react";
 
 const generateDummyItem = (num) => {
-  return new Array(num).fill(null).map((item, index) => `item ${index}`);
+    return new Array(num).fill(null).map((item, index) => `item ${index}`);
 };
 
 const dummyItems = generateDummyItem(10000);
 
 const Example = () => {
-  const [isPending, startTransition] = useTransition();
-  const [filterVal, setFilterVal] = useState("");
+    // const [isPending, startTransition] = useTransition();
+    const [filterVal, setFilterVal] = useState("");
 
-  const changeHandler = (e) => {
-    startTransition(() => {
-      setFilterVal(e.target.value);
-    })
-  };
+    const changeHandler = (e) => {
+        // startTransition(() => {
+        setFilterVal(e.target.value);
+        // });
+    };
 
-  return (
-    <>
-      <input type="text" onChange={changeHandler} />
-      {isPending && <div>Loading...</div>}
-      <ul>
-        {dummyItems
-          .filter((item) => {
+    const filteredItems = dummyItems
+        .filter((item) => {
             if (filterVal === "") return true;
             return item.includes(filterVal);
-          })
-          .map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-      </ul>
-    </>
-  );
+        })
+        .map((item) => <li key={item}>{item}</li>);
+
+    // 遅延させたい値をラップする
+    // input の value は changeHandler によりすぐに表示されるが、
+    // deferredItems は遅延して画面に表示される
+    const deferredItems = useDeferredValue(filteredItems);
+
+    return (
+        <>
+            <input type="text" onChange={changeHandler} />
+            {/* {isPending && <div>Loading...</div>} */}
+            {/* <ul>{filteredItems}</ul> */}
+            <ul>{deferredItems}</ul>
+        </>
+    );
 };
 
 export default Example;
