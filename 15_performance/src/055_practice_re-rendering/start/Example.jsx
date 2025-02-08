da7a31be4e4994f47665d7c2f96cde946c66d4b7
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-08 20:01:56 +0900

    React.memoとuseCallbackに関して改めてまとめ

--- 15_performance/src/055_practice_re-rendering/start/Example.jsx
+++ 15_performance/src/055_practice_re-rendering/start/Example.jsx
@@ -2,6 +2,25 @@ import React, { useCallback, useState } from "react";
 import Child from "./Child";
 import "./Example.css";
 
+/**
+ * React.memo
+ * 親コンポーネントが再レンダリングがされた際に、子コンポーネントも再レンダリングされるが、
+ * React.memo()の引数に渡した子コンポーネントの props でわたってきている値が同一であれば、
+ * 再レンダリングをスキップする事ができる
+ *
+ * useCallback
+ * 親コンポーネントで定義した関数を、React.memoでラップした子コンポーネントに props で渡している場合、
+ * 親コンポーネントが再レンダリングされたタイミングで、関数が再レンダリングされ、
+ * 中身が同一だが、参照先の異なる別の関数(オブジェクト)として定義される為、
+ * React.memoでは、同一の値と認識されず、再レンダリングが発生してしまう。
+ *
+ * 上記のような場合に、親コンポーネントでuseCallback(処理,[依存配列])として関数を定義することで、
+ * 親コンポーネントが再レンダリングされても、依存配列で指定した値が更新されない限り、
+ * useCallbackは再レンダリングされない。
+ * また、React上に保存している同一の関数が props で渡される事で、
+ * 子コンポーネントでは React.memo により、再レンダリングがスキップされる。
+ */
+
 const Example = () => {
     console.log("Parent render");
     const [countA, setCountA] = useState(0);
