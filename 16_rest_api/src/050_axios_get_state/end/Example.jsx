440bc9bc1c0302947a1a9a6fe723633052390970
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-11 07:01:42 +0900

    postManからリクエストを送信する

--- 16_rest_api/src/050_axios_get_state/end/Example.jsx
+++ 16_rest_api/src/050_axios_get_state/end/Example.jsx
@@ -3,30 +3,29 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Example = () => {
+    const [users, setUsers] = useState();
 
-  const [ users, setUsers ] = useState()
+    useEffect(() => {
+        const getUser = async () => {
+            const res = await axios.get("http://localhost:3003/user");
+            setUsers(res.data);
+        };
+        getUser();
+    }, []);
 
-  useEffect(() => {
-    const getUser = async () => {
-      const res = await axios.get('http://localhost:3003/user')
-      setUsers(res.data);
-    }
-    getUser();
-  }, []);
-
-  return (
-    <div>
-      {users?.map(user => {
-        return (
-          <div key={user.id}>
-            <h3>{user.username}</h3>
-            <p>age: {user.age}</p>
-            <p>hobby: {user.hobbies.join(',')}</p>
-          </div>
-        )
-      })}
-    </div>
-  )
+    return (
+        <div>
+            {users?.map((user) => {
+                return (
+                    <div key={user.username}>
+                        <h3>{user.username}</h3>
+                        <p>age: {user.age}</p>
+                        <p>hobby: {user.hobbies.join(",")}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Example;
--- 16_rest_api/src/050_axios_get_state/start/Example.jsx
+++ 16_rest_api/src/050_axios_get_state/start/Example.jsx
@@ -1,5 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Example = () => {
-  return <></>;
+    // const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        const getUser = async () => {
+            const res = await axios.get("http://localhost:3003/user");
+            setUsers(res.data);
+        };
+        getUser();
+    }, []);
+    return (
+        <div>
+            {/* 初回読み込み時、getUser()をawait している状態で、mapを呼び出すと、
+                usersがundefined になり mapが実行できずエラーになる為、以下いずれかの処理が必要
+                1. useStateの初期値を空配列として定義する
+                2. users?.map としてオプショナルチューニング演算子でmapを実行する */}
+
+            {/* {users.map((user) => { */}
+            {users?.map((user) => {
+                return (
+                    <div key={user.id}>
+                        <h3>{user.username}</h3>
+                        <p>age: {user.age}</p>
+                        <p>hobbies: {user.hobbies.join(",")}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Example;
