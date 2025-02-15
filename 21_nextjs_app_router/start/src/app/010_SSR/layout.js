8529c8234afae5a454982ee37d76411e577f3749
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-16 06:37:59 +0900

    :sparkles: ssrの読み込みと csrの読み込み

--- 21_nextjs_app_router/start/src/app/010_SSR/layout.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/layout.js
@@ -1,6 +1,12 @@
 export default function SubLayout({ children }) {
     return (
-        <div style={{ background: "red", color: "white", padding: "3px" }}>
+        <div
+            style={{
+                background: "rgba(255, 60, 0)",
+                color: "white",
+                padding: "3px",
+            }}
+        >
             {children}
         </div>
     );
--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,11 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
 // export default SSR = () => {
 //     return <div> SSR Page</div>;
 // };
 export default function SSR() {
+    const [state, setState] = useState(undefined);
+    useEffect(() => {
+        setState("client loaded");
+    }, []);
+
     return (
         <div>
-            http://localhost:4020/010_SSR のページ
-            <p>/layout.js と/010_SSR/layout.js のスタイルが適用される</p>
+            <h1>http://localhost:4020/010_SSR のページ</h1>
+            <div>
+                <h2>layout.js の適用範囲</h2>
+                <ul>
+                    <li>
+                        /layout.js と/010_SSR/layout.js のスタイルが適用される
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2>use Client の実装</h2>
+                <ul>
+                    <li>
+                        client loadedは CSR で実装されており、
+                        ページ読み込み後に表示されるまで少しだけ遅延してから画面に表示される。
+                    </li>
+                </ul>
+                <div style={{ background: "white", color: "black" }}>
+                    <h2>use client で読み込んだ領域</h2>
+                    <span style={{ color: "red" }}>{state}</span>
+                </div>
+            </div>
         </div>
     );
 }
