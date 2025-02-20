8f31b33fbda96cc3d4e26d53dd2416996a34016d
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-21 06:08:51 +0900

    :sparkles: api get request作成

--- 21_nextjs_app_router/start/src/app/api/article/route.js
+++ 21_nextjs_app_router/start/src/app/api/article/route.js
@@ -1,7 +1,21 @@
 // API Routes
 // https://nextjs.org/docs/app/api-reference/file-conventions/route
+
+/**
+ * API フォルダに route.js を作成9する
+ * ※ APIフォルダを/として、内部に/article/route.js と作成すると、
+ *  http://ENDPOINT/articleの Path にリクエストが来た場合に、
+ *  リクエストの method に応じた、route.js 内の関数が、実行される。
+ * その中に、
+ */
+
 import { ENDPOINT } from "@/constants";
 
-export async function GET() {}
+export async function GET() {
+    const data = await fetch(ENDPOINT).then((res) => res.json());
+    console.log("🚀 ~ GET ~ data:", data);
+
+    return Response.json(data);
+}
 
 export async function POST(request) {}
