7a4b412080291716cbc1895fcc72f9a9f6a7e557
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-27 06:12:38 +0900

    :tada: プリミティブ型

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,32 @@
 const Example = () => {
     // 練習用
+    let str: string = "";
+    str = "Hello";
+    console.log("🚀 ~ Example ~ str:", str);
+
+    let num: number;
+    num = 123;
+    console.log("🚀 ~ Example ~ num:", num);
+
+    let bignum: bigint = 103n;
+    console.log("🚀 ~ Example ~ bignum:", bignum);
+
+    let bool: boolean = true;
+    console.log("🚀 ~ bool:", bool);
+
+    let nullis: null = null;
+    console.log("🚀 ~ Example ~ nullis:", nullis);
+
+    //リテラル型
+    let trueVal: true = true;
+    console.log("🚀 ~ Example ~ trueVal:", trueVal);
+
+    let num123: 123 = 123;
+    console.log("🚀 ~ Example ~ num123:", num123);
+    // num123 = 111; // 123意外は入れられない
+
+    let strHello: "Hello" = "Hello";
+    console.log("🚀 ~ Example ~ strHello:", strHello);
 };
 
 export default Example;
