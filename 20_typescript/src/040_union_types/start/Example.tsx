0e5002a6f1e8f54d2d80200041a6fe43fe20a68d
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-27 20:37:55 +0900

    :sparkles: Union型の定義

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,21 @@
 const Example = () => {
     // 練習用
+    let strOrNum: string | number = "Hello";
+    strOrNum = 111;
+    console.log("🚀 ~ Example ~ strOrNum:", strOrNum);
+
+    let strOrNumOrBool: string | number | boolean = true;
+    console.log("🚀 ~ Example ~ strOrNumOrBool:", strOrNumOrBool);
+
+    type HelloOrNum = "Hello" | number;
+    let hello: HelloOrNum = "Hello";
+    hello = 123;
+    console.log("🚀 ~ Example ~ hello:", hello);
+
+    type DayOfWeek = "Monday" | "Tuesday";
+
+    let day: DayOfWeek = "Tuesday";
+    console.log("🚀 ~ Example ~ day:", day);
 };
 
 export default Example;
