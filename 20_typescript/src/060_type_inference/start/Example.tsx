304b2dc1e6cf89e1b9de35b217bb1990cec8c393
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-28 06:01:01 +0900

    :sparkles: 型推論

--- 20_typescript/src/060_type_inference/start/Example.tsx
+++ 20_typescript/src/060_type_inference/start/Example.tsx
@@ -1,5 +1,21 @@
 const Example = () => {
     // 練習用
+    let str = "hello";
+    let num = 123;
+
+    // const で定義すると、型推論がリテラル型になる
+    const name = "hoge"; // const name: "hoge" で型推論される
+    const age = 32; // const age: 32 で型推論される
+
+    // name = "moge"; // リテラル型なので、'hoge' 意外は代入できない。
+
+    /** オブジェクトの型推論を用いるか否かの基準
+     *
+     * 1. 外部から仕様される物
+     *  ※ 例) class にわたすオプションなどは型定義が必要
+     *
+     * この関数やClass内で利用するものは、簡易なものだれば方の定義は不要
+     */
 };
 
 export default Example;
