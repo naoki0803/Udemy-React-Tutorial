9864358c9a40dfc44ed16683caf976f32244dfac
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-02-28 06:16:14 +0900

    :sparkles: typeを用いた型エイリアスの定義

--- 20_typescript/src/070_type_alias/start/Example.tsx
+++ 20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,30 @@
 const Example = () => {
     // 練習用
+    type User = {
+        name: string;
+        age: number;
+    };
+
+    const user1: User = {
+        name: "hoge",
+        age: 35,
+    };
+
+    type UserName = string;
+    type UserAge = number;
+    type UserGender = "man" | "woman" | "others";
+
+    type UserProfile = {
+        name: UserName;
+        age: UserAge;
+        gender: UserGender;
+    };
+
+    const user2: UserProfile = {
+        name: "hoge",
+        age: 23,
+        gender: "woman",
+    };
 };
 
 export default Example;
