ba2878b829c60a8d09a32b3e7f3d3c91b1b8c476
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-03-01 19:55:06 +0900

    :sparkles: ジェネリクス型の定義

--- 20_typescript/src/090_generics/start/Example.ts
+++ 20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,35 @@
 const Example = () => {
-    // 練習用
+    /** ジェネリクス型とは
+     *  処理内容は同じだが、引数と戻り値の｢型のみ｣異なる場合に、
+     *  ジェネリクス型を用いて型を抽象化することで、汎用的に利用できる型を実装すること。
+     */
+
+    // Good な実装 (ジェネリクス型を用いた実装)
+    // 共通している第一引数の型と、戻り値の型を T っという抽象的な型として定義する
+    const repeat = <T>(value: T, times: number): T[] => {
+        return new Array(times).fill(value);
+    };
+
+    // ジェネリクス型の利用方法
+    const strArray = repeat<string>("7", 3);
+    console.log("🚀 ~ Example ~ strArray:", strArray);
+
+    const numArray = repeat<number>(7, 3);
+    console.log("🚀 ~ Example ~ numArray:", numArray);
+
+    // ジェネリクス型ではない冗長な実装
+    const repeatStr = (value: string, times: number): string[] => {
+        return new Array(times).fill(value);
+    };
+    const repeatNum = (value: number, times: number): number[] => {
+        return new Array(times).fill(value);
+    };
+
+    const strArr = repeatStr("あああ", 5);
+    console.log(strArr);
+
+    const numArr = repeatNum(5, 5);
+    console.log(numArr);
 };
 
 export default Example;
